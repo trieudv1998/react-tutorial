035f7a123b502b7e5edb8bdd269f657cd1bb7b69
cuong1607 <dovancuong160700@gmail.com>
2022-12-07 17:05:31 +0700

    useReduces

---
+++ src/ui-view/page/hooks/useContext-hooks/index.js
@@ -0,0 +1,12 @@
+
+
+const PageUseContext = (props) => {
+   
+
+    return (
+        <>
+                <div>cuong dz</div>
+        </>
+    )
+}
+export default PageUseContext;
---
+++ src/ui-view/page/hooks/useReduces-hooks/index.jsx
@@ -0,0 +1,88 @@
+import React, { useReducer, useRef } from "react";
+import { formReducer, INITIAL_STATE } from "./formReducer";
+import './styles.css';
+
+const Form = () => {
+
+    // USING USEREDUCER
+
+    const [state, dispatch] = useReducer(formReducer, INITIAL_STATE);
+    const tagRef = useRef();
+
+    const handleChange = (e) => {
+        dispatch({
+            type: "CHANGE_INPUT",
+            payload: { name: e.target.name, value: e.target.value },
+        });
+    };
+
+    const handleTags = () => {
+        const tags = tagRef.current.value.split(",");
+        tags.forEach((tag) => {
+            dispatch({ type: "ADD_TAG", payload: tag });
+        });
+    };
+
+    return (
+
+        //USING USEREDUCER
+
+        <div>
+            <form>
+                <input
+                    type="text"
+                    placeholder="Title"
+                    onChange={handleChange}
+                    name="title"
+                />
+                <input
+                    type="text"
+                    placeholder="Desc"
+                    onChange={handleChange}
+                    name="desc"
+                />
+                <input
+                    type="number"
+                    placeholder="Price"
+                    onChange={handleChange}
+                    name="price"
+                />
+                <p>Category:</p>
+                <select onChange={handleChange} name="category">
+                    <option value="sneakers">Sneakers</option>
+                    <option value="tshirts">T-shirts</option>
+                    <option value="jeans">Jeans</option>
+                </select>
+                <p>Tags:</p>
+                <textarea
+                    ref={tagRef}
+                    placeholder="Seperate tags with commas..."
+                ></textarea>
+                <button onClick={handleTags} type="button">
+                    Add Tags
+                </button>
+                <div className="tags">
+                    {state.tags.map((tag) => (
+                        <small
+                            onClick={() => dispatch({ type: "REMOVE_TAG", payload: tag })}
+                            key={tag}
+                        >
+                            {tag}
+                        </small>
+                    ))}
+                </div>
+                <div className="quantity">
+                    <button onClick={() => dispatch({ type: "DECREASE" })} type="button">
+                        -
+                    </button>
+                    <span>Quantity ({state.quantity})</span>
+                    <button onClick={() => dispatch({ type: "INCREASE" })} type="button">
+                        +
+                    </button>
+                </div>
+            </form>
+        </div>
+    );
+};
+
+export default Form;
